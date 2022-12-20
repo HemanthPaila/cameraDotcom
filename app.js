63e5d107953c37ee05e765987b14ab2f3cf3cae5
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-20 21:55:05 +0530

    just finished a home layout

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("home");
 });
+app.get("/camera", function (req, res) {
+  res.render("camera");
+});
 app.listen(3001, function () {
   console.log("server running at port numver 3001!");
 });
