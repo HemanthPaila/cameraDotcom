02e27259cce6d2a6f710d72bef07db9935db5b38
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-22 22:47:24 +0530

    added a module to the project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
   res.render("home");
 });
