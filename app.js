0aba5ad761af10dc2ca7986f98bda3dc243fd31f
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-23 21:49:11 +0530

    last commit for the day

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
   res.render("home");
 });
 app.get("/camera", function (req, res) {
   res.render("camera");
+});
+app.get("/camerasetup", function (req, res) {
+  res.render("camerasetup");
+});
+app.post("/update", function (req, res) {
+  var name = req.body.name;
+  var age = req.body.age;
+  var camera = req.body.camera;
+  var color = req.body.color;
+  // console.log(name + " " + age + " " + camera + " " + color);
+  res.redirect("camerasetup");
 });
 app.listen(3001, function () {
   console.log("server running at port numver 3001!");
