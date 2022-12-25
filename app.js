8118f11f5b609e2a28602adba4c9c0e77e026e6c
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-25 23:46:13 +0530

    added login for web

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ app.set("view engine", "ejs");
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
+  localhost: "3306",
+  user: "root", // your root username
+  password: "Hemanth@123",
+  database: "cameradotcom", // the name of your db
+});
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -13,13 +20,61 @@ app.get("/camera", function (req, res) {
 app.get("/camerasetup", function (req, res) {
   res.render("camerasetup");
 });
+app.get("/profile", function (req, res) {
+  res.render("profile");
+});
 app.post("/update", function (req, res) {
-  var name = req.body.name;
-  var age = req.body.age;
-  var camera = req.body.camera;
-  var color = req.body.color;
+  var data = {
+    name: req.body.name,
+    age: req.body.age,
+    camera: req.body.camera,
+    color: req.body.color,
+  };
   // console.log(name + " " + age + " " + camera + " " + color);
-  res.redirect("camerasetup");
+  res.render("card", {
+    personname: data.name,
+    personage: data.age,
+    personcamera: data.camera,
+    personcolor: data.color,
+  });
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.post("/signup", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  connection.query(
+    "select * from signin where username=? and password=?",
+    [username, password],
+    function (error, result, fields) {
+      if (result.length > 0) {
+        res.render("signin");
+      } else {
+        res.redirect("/");
+      }
+      res.end();
+    }
+  );
+});
+app.post("/signin", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  connection.query(
+    "insert into signin(username,password) values(?,?)",
+    [username, password],
+    function (error, result, fields) {
+      if (!error) {
+        res.redirect("/home");
+      } else {
+        res.redirect("/");
+      }
+      res.end();
+    }
+  );
+});
+app.get("/home", function (req, res) {
+  res.render("home");
 });
 app.listen(3001, function () {
   console.log("server running at port numver 3001!");
